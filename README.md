c90be07905f02e3440073ba849d92a84517b5d39
YanpingDong <lear521@163.com>
2018-01-04 00:51:56 +0800

    add optimal way for two sum

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 代码在 Python 2.7.10 环境下测试通过
 
+# Two Sum
+
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+```
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+```
+
 # Add Binary
 
 Given two binary strings, return their sum (also a binary string).
--- TwoSum.py
+++ TwoSum.py
@@ -13,7 +13,27 @@ class TwoSum(object):
                 if target == firstNum + secondNum:
                     return [i,k]
 
+    def twoSumOptimal(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        remainder={}
+        numsLen = len(nums)
+        for i in range(0, numsLen):
+            num = nums[i]
+            print remainder.get(num,False)
+            if(remainder.get(num) or 0 == remainder.get(num)):
+                return [remainder[num], i]
+            else:
+                remainder[target-num] = i
+
+
 
 if __name__ == '__main__':
     ts = TwoSum()
-    print ts.twoSum([3,2,4],6)
+    print ts.twoSum([3, 2, 4], 6)
+    print ts.twoSumOptimal([2, 5, 5, 11], 10)
+    print ts.twoSumOptimal([3, 2, 4], 6)
+    print ts.twoSumOptimal([3, 3], 6)
